8e1fd0d3c21beacc9cd3fcb57cf142177af86c93
CodeMaker_4 <codemaker04@gmail.com>
2022-04-25 22:18:57 +0200

    init

---
+++ cmvec2d.js
@@ -0,0 +1,304 @@
+/** 
+ * Class representing a 2d vector.
+ * @example
+ * new Vector2d(1, 1).mag() === Math.sqrt(2)
+ * @example
+ * let a = new Vector2d(4, 0); // create an X axis
+ * let b = new Vector2d(0, 3); // create an Y axis
+ * let c = a.copy().add(b).mag(); // create a diagonal vector, and calculate its magnitude
+ * c == 5
+ * // a^2 + b^2 = c^2
+ * @version 1.0
+ * @author CodeMaker_4
+*/
+class Vector2d {
+    /**
+     * Create a new 2d vector with a given x and y.
+     * 
+     * To copy a vector use vec2d.copy();
+     * 
+     * @param {number} x The initial X component of the vector
+     * @param {number} y The initial Y component of the vector.
+     */
+    constructor(x, y) {
+        /**
+         * The x component of this vector.
+         * @type {number}
+         */
+        this.x = x;
+        /**
+         * The y component of this vector.
+         * @type {number}
+         */
+        this.y = y;
+    }
+
+    /**
+     * Create a new Vector2d pointing in a random direction and with a given magnitude.
+     * @param {number} [magnitude=1] - The magnitude of the new Vector2d. (default = 1)
+     * @returns {Vector2d} The new vector.
+     */
+    static randAngle(magnitude = 1) {
+        let angle = Math.random() * Math.PI * 2;
+        return new Vector(Math.cos(angle)*magnitude, Math.sin(angle)*magnitude);
+    }
+
+    /**
+     * Create a new Vector2d pointing in a random direction and with a random magnitude.
+     * @param {number} maxMagnitude - The maximum magnitude of the new Vector2d. (default = 1)
+     * @returns {Vector2d} The new vector.
+     */
+    static rand2D(maxMagnitude = 1) {
+        return Vector.randAngle().mult(Math.random() * maxMagnitude);
+    }
+
+    /**
+     * Create a new Vector2d with the x and y components on a random position in a box.
+     * 
+     * One corner of the box is the origin, the other is at (width,height)
+     * 
+     * @param {number} width - The maximum value for the X component.
+     * @param {number} [height=width] - The maximum value for the y component. (default = width)
+     * @returns {Vector2d} The new vector.
+     */
+    static randBox(width, height = width) {
+        return new Vector(Math.random()*width, Math.random()*height);
+    }
+
+    /**
+     * Create and return a new Vector2d instance with the same x and y component values.
+     * @returns {Vector2d} The copied vector.
+     */
+    copy() {
+        return new Vector2d(this.x, this.y);
+    }
+
+    /**
+     * Copy the x and y component values from a given vector into this vector, overwriting the old values.
+     * @param {Vector2d} other The vector to copy the component values from.
+     * @returns {Vector2d} this
+     */
+    set(other) {
+        this.x = other.x;
+        this.y = other.y;
+        return this; 
+    }
+
+    /**
+     * Add a given vector to this vector. This changes this vector, but leaves the other intact.
+     * @param {Vector2d} other The vector to add to this vector.
+     * @returns {Vector2d} this
+     */
+    add(other) {
+        this.x += other.x;
+        this.y += other.y;
+        return this;
+    }
+
+    /**
+     * Subtract a given vector from this vector. This changes this vector, but leaves the other intact.
+     * @param {Vector2d} other The vector to subtract from this vector.
+     * @returns {Vector2d} this
+     */
+    sub(other) {
+        this.x -= other.x;
+        this.y -= other.y;
+        return this;
+    }
+
+    /**
+     * Multiply this vector by a given number.
+     * @param {number} num The number to multiply with.
+     * @returns {Vector2d} this
+     */
+    mult(num) {
+        this.x = this.x*num;
+        this.y = this.y*num;
+        return this;
+    }
+
+    /**
+     * Divide this vector by a given number.
+     * @param {number} num The number to divide with.
+     * @returns {Vector2d} this
+     */
+    div(num) {
+        this.x = this.x/num;
+        this.y = this.y/num;
+        return this;
+    }
+
+    /**
+     * Add seperate x and y component numbers to this vector.
+     * @param {number} x The x component to add.
+     * @param {number} y The y component to add.
+     * @returns {Vector2d} this
+     */
+    addXY(x, y) {
+        this.x += x;
+        this.y += y;
+        return this;
+    }
+
+    /**
+     * Subtract seperate x and y component numbers from this vector.
+     * @param {number} x The x component to subtract.
+     * @param {number} y The y component to subtract.
+     * @returns {Vector2d} this
+     */
+    subXY(x, y) {
+        this.x -= x;
+        this.y -= y;
+        return this;
+    }
+
+    /**
+     * Calculate the squared magnitude of this vector.
+     * @returns {number} The squared magnitude of this vector.
+     */
+    sqMag() {
+        return this.x**2 + this.y**2;
+    }
+
+    /**
+     * Calculate the magnitude of this vector.
+     * @returns {number} The magnitude of this vector.
+     */
+    mag() {
+        return Math.sqrt(this.sqMag());
+    }
+
+    /**
+     * Calculate the distance between this vector and another vector.
+     * @param {Vector2d} other The other vector
+     * @returns {number} The distance between the vectors.
+     */
+    dist(other) {
+        return this.copy().sub(other).mag();
+    }
+
+    /**
+     * Set the magnitude of this vector to a given number.
+     * @param {number} newMag The new magnitude for this vector.
+     * @returns {Vector2d} this
+     */
+    setMag(newMag) {
+        let mag = this.mag();
+        if (mag == 0) { // if no direction make up something random
+            this.set(Vector.randAngle().mult(newMag));
+            return this;
+        }
+        this.mult(newMag/this.mag());
+        return this;
+    }
+
+    /**
+     * Set the magnitude of this vector to 1.
+     * @returns {Vector2d} this
+     */
+    normalize() {
+        this.setMag(1);
+        return this;
+    }
+
+    /**
+     * Round the x and y components downwards.
+     * @returns {Vector2d} this
+     */
+    floor() {
+        this.x = Math.floor(this.x);
+        this.y = Math.floor(this.y);
+        return this;
+    }
+
+    /**
+     * Round the x and y components to the nearest integer.
+     * @returns {Vector2d} this
+     */
+    round() {
+        this.x = Math.round(this.x);
+        this.y = Math.round(this.y);
+        return this;
+    }
+
+    /**
+     * Calculate the angle of this vector.
+     * @returns {number} The angle.
+     */
+    getAngle() {
+        return Math.atan2(this.y, this.x);
+    }
+
+    /**
+     * Rotate this vector such that it has the given angle.
+     * @param {number} angle The angle this vector should have.
+     * @returns {Vector2d} this
+     */
+    setAngle(angle) {
+        let oldMag = this.mag();
+        this.x = Math.cos(angle)*oldMag;
+        this.y = Math.sin(angle)*oldMag;
+        return this;
+    }
+
+    /**
+     * Rotate this vector by a given amount.
+     * @param {number} angle The amount to rotate by.
+     * @returns {Vector2d} this
+     */
+    rotate(angle) {
+        this.setAngle(this.getAngle() + angle);
+        return this;
+    }
+
+    /**
+     * Calculate the dot product of this vector and another vector.
+     * @param {Vector2d} other The other vector.
+     * @returns {number} number
+     */
+    dot(other) {
+        return this.x*other.x + this.y*other.y;
+    }
+
+    /**
+     * Check if one of the components is NaN. Handy for error assertion.
+     * @returns {boolean} True if there is a NaN.
+     */
+    hasNaN() {
+        return isNaN(this.x) || isNaN(this.y);
+    }
+
+    /**
+     * Assert if there are NaNs in the components of this vector. Throws an error if it found NaN.
+     * @returns {void} Void. Throws an error if there is a NaN.
+     */
+    assertNaN() {
+        if (this.hasNaN()) throw "found NaN";
+    }
+
+    /**
+     * Check if the vector is within a given rectangle starting at the origin, and spanning towards (width,height). Also has an optional radius argument, which decreases the bounds by the given amount, handy for checking if a ball is inside a rect.
+     * @param {number} width Width of the rect.
+     * @param {number} height Height of the rect.
+     * @param {number} [radius=0] Radius of the sphere around the vector that must be fully inside. (default=0)
+     * @returns {boolean} True if the vector is inside the rect.
+     */
+    isInRect(width, height, radius = 0) {
+        return (this.x > radius && this.y > radius && this.x < width-radius && this.y < height-radius);
+    }
+
+    /**
+     * Ensure that the vector is within a given rectangle starting at the origin, and spanning towards (width,height). Also has an optional radius argument, which decreases the bounds by the given amount, handy for ensuring that a ball is inside a rect.
+     * @param {number} width Width of the rect.
+     * @param {number} height Height of the rect.
+     * @param {number} [radius=0] Radius of the sphere around the vector that must be fully inside.  
+     * @returns this
+     */
+    putInRect(width, height, radius) {
+        if (this.x < radius) this.x = radius;
+        if (this.y < radius) this.y = radius;
+        if (this.x > width-radius) this.x = width-radius;
+        if (this.y > height-radius) this.y = height-radius;
+        return this;
+    }
+}
