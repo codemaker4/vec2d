1c5574049a040ebf2934344028edae1f9427fc85
codemaker4 <thijs.rutledge@coderclass.nl>
2022-04-27 16:42:10 +0200

    renamed the class and a few files

--- README.md
+++ README.md
@@ -5,21 +5,21 @@ A simple 2d vector library for JavaScript.
 Either download cmvec2d.js or import the script with this script tag:
 
 ```html
-<script src="https://raw.githubusercontent.com/codemaker4/vec2d/master/cmvec2d.js"></script>
+<script src="https://raw.githubusercontent.com/codemaker4/vec2d/master/vec2d.js"></script>
 ```
 
 ## examples
 ```js
-let a = new Vector2d(4,0) // create a vector of length 4
-let b = new Vector2d(0,3) // create a purpendicular vector of length 3
+let a = new Vec2d(4,0) // create a vector of length 4
+let b = new Vec2d(0,3) // create a purpendicular vector of length 3
 let c = a.copy().add(b) // create a new diagonal vector
 console.log(c.mag()) // 5
 // a^2 + b^2 = c^2
 ```
 ```js
-let pos = new Vector2d(0,0) // define a postiion
-let vel = new Vector2d(0,0) // define a velocity
-let grav = new Vector2d(0,9.8) // define a gravitational force
+let pos = new Vec2d(0,0) // define a postiion
+let vel = new Vec2d(0,0) // define a velocity
+let grav = new Vec2d(0,9.8) // define a gravitational force
 
 setInterval(()=>{
   vel.add(grav) // apply the force
--- vec2d.js
+++ vec2d.js
@@ -1,17 +1,17 @@
 /** 
  * Class representing a 2d vector.
  * @example
- * new Vector2d(1, 1).mag() === Math.sqrt(2)
+ * new Vec2d(1, 1).mag() === Math.sqrt(2)
  * @example
- * let a = new Vector2d(4, 0); // create an X axis
- * let b = new Vector2d(0, 3); // create an Y axis
+ * let a = new Vec2d(4, 0); // create an X axis
+ * let b = new Vec2d(0, 3); // create an Y axis
  * let c = a.copy().add(b).mag(); // create a diagonal vector, and calculate its magnitude
  * c == 5
  * // a^2 + b^2 = c^2
  * @version 1.0
  * @author CodeMaker_4
 */
-class Vector2d {
+class Vec2d {
     /**
      * Create a new 2d vector with a given x and y.
      * 
@@ -34,9 +34,9 @@ class Vector2d {
     }
 
     /**
-     * Create a new Vector2d pointing in a random direction and with a given magnitude.
-     * @param {number} [magnitude=1] - The magnitude of the new Vector2d. (default = 1)
-     * @returns {Vector2d} The new vector.
+     * Create a new Vec2d pointing in a random direction and with a given magnitude.
+     * @param {number} [magnitude=1] - The magnitude of the new Vec2d. (default = 1)
+     * @returns {Vec2d} The new vector.
      */
     static randAngle(magnitude = 1) {
         let angle = Math.random() * Math.PI * 2;
@@ -44,39 +44,39 @@ class Vector2d {
     }
 
     /**
-     * Create a new Vector2d pointing in a random direction and with a random magnitude.
-     * @param {number} maxMagnitude - The maximum magnitude of the new Vector2d. (default = 1)
-     * @returns {Vector2d} The new vector.
+     * Create a new Vec2d pointing in a random direction and with a random magnitude.
+     * @param {number} maxMagnitude - The maximum magnitude of the new Vec2d. (default = 1)
+     * @returns {Vec2d} The new vector.
      */
     static rand2D(maxMagnitude = 1) {
         return Vector.randAngle().mult(Math.random() * maxMagnitude);
     }
 
     /**
-     * Create a new Vector2d with the x and y components on a random position in a box.
+     * Create a new Vec2d with the x and y components on a random position in a box.
      * 
      * One corner of the box is the origin, the other is at (width,height)
      * 
      * @param {number} width - The maximum value for the X component.
      * @param {number} [height=width] - The maximum value for the y component. (default = width)
-     * @returns {Vector2d} The new vector.
+     * @returns {Vec2d} The new vector.
      */
     static randBox(width, height = width) {
         return new Vector(Math.random()*width, Math.random()*height);
     }
 
     /**
-     * Create and return a new Vector2d instance with the same x and y component values.
-     * @returns {Vector2d} The copied vector.
+     * Create and return a new Vec2d instance with the same x and y component values.
+     * @returns {Vec2d} The copied vector.
      */
     copy() {
-        return new Vector2d(this.x, this.y);
+        return new Vec2d(this.x, this.y);
     }
 
     /**
      * Copy the x and y component values from a given vector into this vector, overwriting the old values.
-     * @param {Vector2d} other The vector to copy the component values from.
-     * @returns {Vector2d} this
+     * @param {Vec2d} other The vector to copy the component values from.
+     * @returns {Vec2d} this
      */
     set(other) {
         this.x = other.x;
@@ -86,8 +86,8 @@ class Vector2d {
 
     /**
      * Add a given vector to this vector. This changes this vector, but leaves the other intact.
-     * @param {Vector2d} other The vector to add to this vector.
-     * @returns {Vector2d} this
+     * @param {Vec2d} other The vector to add to this vector.
+     * @returns {Vec2d} this
      */
     add(other) {
         this.x += other.x;
@@ -97,8 +97,8 @@ class Vector2d {
 
     /**
      * Subtract a given vector from this vector. This changes this vector, but leaves the other intact.
-     * @param {Vector2d} other The vector to subtract from this vector.
-     * @returns {Vector2d} this
+     * @param {Vec2d} other The vector to subtract from this vector.
+     * @returns {Vec2d} this
      */
     sub(other) {
         this.x -= other.x;
@@ -109,7 +109,7 @@ class Vector2d {
     /**
      * Multiply this vector by a given number.
      * @param {number} num The number to multiply with.
-     * @returns {Vector2d} this
+     * @returns {Vec2d} this
      */
     mult(num) {
         this.x = this.x*num;
@@ -120,7 +120,7 @@ class Vector2d {
     /**
      * Divide this vector by a given number.
      * @param {number} num The number to divide with.
-     * @returns {Vector2d} this
+     * @returns {Vec2d} this
      */
     div(num) {
         this.x = this.x/num;
@@ -132,7 +132,7 @@ class Vector2d {
      * Add seperate x and y component numbers to this vector.
      * @param {number} x The x component to add.
      * @param {number} y The y component to add.
-     * @returns {Vector2d} this
+     * @returns {Vec2d} this
      */
     addXY(x, y) {
         this.x += x;
@@ -144,7 +144,7 @@ class Vector2d {
      * Subtract seperate x and y component numbers from this vector.
      * @param {number} x The x component to subtract.
      * @param {number} y The y component to subtract.
-     * @returns {Vector2d} this
+     * @returns {Vec2d} this
      */
     subXY(x, y) {
         this.x -= x;
@@ -170,7 +170,7 @@ class Vector2d {
 
     /**
      * Calculate the distance between this vector and another vector.
-     * @param {Vector2d} other The other vector
+     * @param {Vec2d} other The other vector
      * @returns {number} The distance between the vectors.
      */
     dist(other) {
@@ -180,7 +180,7 @@ class Vector2d {
     /**
      * Set the magnitude of this vector to a given number.
      * @param {number} newMag The new magnitude for this vector.
-     * @returns {Vector2d} this
+     * @returns {Vec2d} this
      */
     setMag(newMag) {
         let mag = this.mag();
@@ -194,7 +194,7 @@ class Vector2d {
 
     /**
      * Set the magnitude of this vector to 1.
-     * @returns {Vector2d} this
+     * @returns {Vec2d} this
      */
     normalize() {
         this.setMag(1);
@@ -203,7 +203,7 @@ class Vector2d {
 
     /**
      * Round the x and y components downwards.
-     * @returns {Vector2d} this
+     * @returns {Vec2d} this
      */
     floor() {
         this.x = Math.floor(this.x);
@@ -213,7 +213,7 @@ class Vector2d {
 
     /**
      * Round the x and y components to the nearest integer.
-     * @returns {Vector2d} this
+     * @returns {Vec2d} this
      */
     round() {
         this.x = Math.round(this.x);
@@ -232,7 +232,7 @@ class Vector2d {
     /**
      * Rotate this vector such that it has the given angle.
      * @param {number} angle The angle this vector should have.
-     * @returns {Vector2d} this
+     * @returns {Vec2d} this
      */
     setAngle(angle) {
         let oldMag = this.mag();
@@ -244,7 +244,7 @@ class Vector2d {
     /**
      * Rotate this vector by a given amount.
      * @param {number} angle The amount to rotate by.
-     * @returns {Vector2d} this
+     * @returns {Vec2d} this
      */
     rotate(angle) {
         this.setAngle(this.getAngle() + angle);
@@ -253,7 +253,7 @@ class Vector2d {
 
     /**
      * Calculate the dot product of this vector and another vector.
-     * @param {Vector2d} other The other vector.
+     * @param {Vec2d} other The other vector.
      * @returns {number} number
      */
     dot(other) {
